06b749d4b9606520ecceaefc973547a98150d9d8
Costalesjonatan <42694800+Costalesjonatan@users.noreply.github.com>
2019-03-29 08:18:29 -0300

    Inicio de la clase Juego

---
+++ Ligths out/src/negocio/Juego.java
@@ -0,0 +1,14 @@
+package negocio;
+
+import java.io.IOException;
+
+public class Juego {
+	
+	private Tablero tablero;
+	
+	public Juego() throws IOException
+	{
+		tablero = new Tablero();
+	}
+
+}
